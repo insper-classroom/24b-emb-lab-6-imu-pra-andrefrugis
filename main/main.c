b6cd44b8573dde3e53669b7880f029322c21fec3
andref9 <andref9@al.insper>
2024-09-23 19:16:51 +0000

    .

--- main/main.c
+++ main/main.c
@@ -17,6 +17,13 @@ const int MPU_ADDRESS = 0x68;
 const int I2C_SDA_GPIO = 4;
 const int I2C_SCL_GPIO = 5;
 
+QueueHandle_t xQueueAdc;
+
+typedef struct adc {
+    int axis;
+    int val;
+} adc_t;
+
 static void mpu6050_reset() {
     // Two byte reset. First byte register, second byte data
     // There are a load more options to set up the device in different ways that could be added here
@@ -72,6 +79,8 @@ void mpu6050_task(void *p) {
 
     int16_t acceleration[3], gyro[3], temp;
 
+    adc_t adc;
+
     while(1) {
         mpu6050_read_raw(acceleration, gyro, &temp);
 
@@ -89,22 +98,67 @@ void mpu6050_task(void *p) {
 
         FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, 0.01f); // Sample period of 10ms
         FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
-        printf("Roll %0.1f, Pitch %0.1f, Yaw %0.1f\n", euler.angle.roll, euler.angle.pitch, euler.angle.yaw);
+        euler.angle.roll = -euler.angle.roll;
+        euler.angle.roll = -euler.angle.yaw;
+        //printf("Roll %0.1f, Pitch %0.1f, Yaw %0.1f\n", euler.angle.roll, euler.angle.pitch, euler.angle.yaw);
         
         // printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
         // printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
         // printf("Temp. = %f\n", (temp / 340.0) + 36.53);
 
-        vTaskDelay(pdMS_TO_TICKS(10));
+        if(euler.angle.yaw > 10 || euler.angle.yaw < -10) {
+            adc.axis = 0;
+            adc.val = euler.angle.yaw;
+            xQueueSend(xQueueAdc, &adc, portMAX_DELAY);
+            vTaskDelay(pdMS_TO_TICKS(10));
+        }
+        else if(euler.angle.roll > 10 || euler.angle.roll < -10) {
+            adc.axis = 1;
+            adc.val = euler.angle.roll;
+            xQueueSend(xQueueAdc, &adc, portMAX_DELAY);
+            vTaskDelay(pdMS_TO_TICKS(10));
+        }
     }
+}
 
+void uart_task(void *p) {
+    adc_t adc;
 
+    while (1) {
+        xQueueReceive(xQueueAdc, &adc, portMAX_DELAY);
+
+        // Preparar o cabeçalho de sincronização e o pacote de dados
+        uint8_t packet[4];
+        packet[0] = adc.axis; // AXIS: 0 ou 1
+
+        // VAL_1 e VAL_0 são os dois bytes que representam o valor
+        int16_t value = (int16_t)adc.val; // Conversão para garantir 16 bits
+        packet[1] = (value >> 8) & 0xFF; // Byte mais significativo
+        packet[2] = value & 0xFF; // Byte menos significativo
+
+        // EOP (End of Packet)
+        packet[3] = 0xFF; // -1 em unsigned char é 0xFF
+
+        // Enviar pacote de 4 bytes
+        uart_write_blocking(uart0, packet, 4);
+    }
+} 
+
+void uart_setup() {
+    // Escolha dos pinos para UART0 (exemplo para GPIO0 TX, GPIO1 RX)
+    uart_init(uart0, 115200);  // Inicializa UART0 com 115200 baud
+    gpio_set_function(0, GPIO_FUNC_UART);  // Pino 0 como TX
+    gpio_set_function(1, GPIO_FUNC_UART);  // Pino 1 como RX
 }
 
 int main() {
     stdio_init_all();
+    uart_setup();
+
+    xQueueAdc = xQueueCreate(32, sizeof(adc_t));
 
     xTaskCreate(mpu6050_task, "mpu6050_Task 1", 8192, NULL, 1, NULL);
+    xTaskCreate(uart_task, "uart_Task 2", 8192, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
